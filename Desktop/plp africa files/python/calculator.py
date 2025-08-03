7733b04025cf9b025d54a9d756a84286ca345071
juliette <ndungujuliet20@gmail.com>
2025-08-03 16:37:59 +0300

    add basic python calculator python program

---
+++ Desktop/plp africa files/python/calculator.py
@@ -0,0 +1,24 @@
+# Basic Calculator program
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation. Please enter one of +, -, *, /.")
+
